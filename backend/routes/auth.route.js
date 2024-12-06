d7f04dc621be350c42b0036ff7625a61873cb54f
Ali Mohsin <syedalimohsin987@gmail.com>
2024-12-06 12:05:43 +0500

    1. auth routes and controllers added

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 
-import { login, logout, signup } from "../controllers/auth.contoller.js";
+import { getMe, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
@@ -9,6 +10,8 @@ router.post("/signup", signup);
 router.post("/login", login);
 
 router.post("/logout", logout);
+
+router.get("/me", protectRoute, getMe);
 
 export default router;
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import connectMongoDB from "./db/connectMongoDb.js";
@@ -9,10 +10,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectMongoDB();
 });
-

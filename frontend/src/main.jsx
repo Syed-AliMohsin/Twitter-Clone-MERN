99a97011f3bd9f02d097922c560fbec5abd90704
Ali Mohsin <syedalimohsin987@gmail.com>
2024-12-07 17:14:17 +0500

    3. auth handled and routes protected in client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import "./index.css";
 import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
